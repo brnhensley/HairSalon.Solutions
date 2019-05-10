379550261ce63692cf00b67e5783cfbb8ab59278
Brian Hensley <brnhensley@gmail.com>
2019-05-10 14:31:38 -0700

    I've made no progress at all. Everything I try doesn't work.

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,19 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Diagnostics;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using HairSalon.Models;
+//
+// namespace HairSalon.Controllers
+// {
+//     public class HomeController : Controller
+//     {
+//       [HttpGet("/")]
+//       public ActionResult Index()
+//       {
+//         return View();
+//       }
+//     }
+// }
+
+
 using Microsoft.AspNetCore.Mvc;
-using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
     public class HomeController : Controller
     {
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -21,14 +21,6 @@ namespace HairSalon.Controllers
       return View();
     }
 
-    [HttpPost("/stylists")]
-    public ActionResult Create(string stylistName)
-    {
-      Stylist newStylist = new Stylist(stylistName);
-      List<Stylist> allStylists = Stylist.GetAll();
-      return View("Index", allStylists);
-    }
-
     [HttpGet("/stylists/{id}")]
     public ActionResult Show(int id)
     {
@@ -40,29 +32,35 @@ namespace HairSalon.Controllers
       return View(model);
     }
 
-    // This one creates new Clients within a given Stylist, not new Stylists:
-    [HttpPost("/stylists/{stylistId}/items/new")]
-    public ActionResult AddClient(int stylistId, int clientId)
+    // Add a Stylist
+    [HttpPost("/stylists")]
+    public ActionResult Create(string stylistName)
     {
-      Stylist stylist = Stylist.Find(stylistId);
-      Client newClient = Client.Find(clientId);
-      stylist.AddClient(newClient);
-      return RedirectToAction("Show", new { id = stylistId });
+      Stylist newStylist = new Stylist(stylistName);
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View("Index", allStylists);
     }
 
-
-    // [HttpPost("/stylists/{stylistId}/clients")]
-    // public ActionResult Create(int stylistId, string clientName)
+    [HttpPost("/stylists/{stylistId}/clients")]
+    public ActionResult Create(int stylistId, string clientName)
+    {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Stylist foundStylist = Stylist.Find(stylistId);
+        Client newClient = new Client(clientName, stylistId);
+        newClient.Save();
+        List<Client> stylistClients = foundStylist.GetClients();
+        model.Add("clients", stylistClients);
+        model.Add("stylist", foundStylist);
+        return View("Show", model);
+    }
+    // // This one creates new Clients within a given Stylist, not new Stylists:
+    // [HttpPost("/stylists/{stylistId}/items/new")]
+    // public ActionResult AddClient(int stylistId, int clientId)
     // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   Stylist foundStylist = Stylist.Find(stylistId);
-    //   Client newClient = new Client(clientName, stylistId);
-    //   newClient.Save();
-    //   foundStylist.AddClient(newClient);
-    //   List<Client> stylistClients = foundStylist.GetClients();
-    //   model.Add("clients", stylistClients);
-    //   model.Add("stylists", foundStylist);
-    //   return View("Show", model);
+    //   Stylist stylist = Stylist.Find(stylistId);
+    //   Client newClient = Client.Find(clientId);
+    //   stylist.AddClient(newClient);
+    //   return RedirectToAction("Show", new { id = stylistId });
     // }
 
   }
