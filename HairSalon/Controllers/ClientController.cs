2f701ebcf4ed782ea4f310d37704b8411e027038
Brian Hensley <brnhensley@gmail.com>
2019-05-11 16:27:06 -0700

    fix broken links to client pages and new client page

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -5,21 +5,32 @@ using System.Collections.Generic;
 namespace HairSalon.Controllers
 {
 
-    public class ClientsController : Controller
+  public class ClientsController : Controller
+  {
+    [HttpGet("/stylists/{stylistId}/clients/{clientId}")]
+    public ActionResult Show(int stylistId, int clientId)
     {
-        [HttpGet("/stylists/{stylistId}/clients/new")]
-        public ActionResult New(int stylistId)
-        {
-            Stylist stylist = Stylist.Find(stylistId);
-            return View(stylist);
-        }
-
-        [HttpPost("/stylists")]
-        public ActionResult Create(string name, int id)
-        {
-          Stylist newStylist = new Stylist(name, id);
-          newStylist.Save();
-          return RedirectToAction("Index");
-        }
+      Client client = Client.Find(clientId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist stylist = Stylist.Find(stylistId);
+      model.Add("client", client);
+      model.Add("stylist", stylist);
+      return View(model);
     }
+
+    [HttpGet("/stylists/{stylistId}/clients/new")]
+    public ActionResult New(int stylistId)
+    {
+      Stylist stylist = Stylist.Find(stylistId);
+      return View(stylist);
+    }
+
+    [HttpPost("/stylists")]
+    public ActionResult Create(string name, int id)
+    {
+      Stylist newStylist = new Stylist(name, id);
+      newStylist.Save();
+      return RedirectToAction("Index");
+    }
+  }
 }
