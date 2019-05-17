a667b902c1da7a6e92227779d601b40177790c06
Brian Hensley <brnhensley@gmail.com>
2019-05-17 16:57:36 -0700

    add views and controller for Specialties. They don't work yet and tests still fail

---
+++ HairSalon/Controllers/SpecialtyController.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class SpecialtyController : Controller
+  {
+
+    [HttpGet("/specialties")]
+    public ActionResult Index()
+    {
+      List<Specialty> allSpecialties = Specialty.GetAll();
+      return View(allSpecialties);
+    }
+
+    [HttpGet("/specialties/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/specialties")]
+    public ActionResult Create(string type)
+    {
+      Specialty newSpecialty = new Specialty(type);
+      newSpecialty.Save();
+      List<Specialty> allSpecialties = Specialty.GetAll();
+      return View("Index", allSpecialties);
+    }
+
+    [HttpGet("/specialties/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Specialty selectedSpecialty = Specialty.Find(id);
+      List<Stylist> specialtyStylists = selectedSpecialty.GetStylists();
+      model.Add("specialty", selectedSpecialty);
+      model.Add("specialtyStylists", specialtyStylists);
+      return View(model);
+    }
+
+
+    [HttpPost("/specialties/{specialtyId}/stylists/new")]
+    public ActionResult AddStylist(int specialtyId, int stylistId)
+    {
+      Specialty specialty = Specialty.Find(specialtyId);
+      Stylist stylist = Stylist.Find(stylistId);
+      specialty.AddStylist(stylist);
+      return RedirectToAction("Show",  new { id = specialtyId });
+    }
+
+
+  }
+}
